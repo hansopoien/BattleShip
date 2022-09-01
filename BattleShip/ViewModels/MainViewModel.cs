8ed489d955b9db21a5de1dc689034cd66b33647e
Hans Opöien <haop2100@student.miun.se>
2022-09-01 17:21:42 +0200

    q

--- BattleShip/ViewModels/MainViewModel.cs
+++ BattleShip/ViewModels/MainViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using BattleShip.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,6 @@ namespace BattleShip.ViewModels
 {
     internal class MainViewModel : BaseViewModel
     {
-        public BaseViewModel CurrentViewModel { get; set; } = new GameViewModel(); 
+        public BaseViewModel CurrentViewModel { get; set; } = new GameViewModel();
     }
 }
