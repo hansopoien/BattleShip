4428451031779dcfb268a12edaae0b0b23432d52
Hans Opöien <haop2100@student.miun.se>
2022-09-01 16:01:46 +0200

    Add project files.

---
+++ BattleShip/ViewModels/GameViewModel.cs
@@ -0,0 +1,13 @@
+﻿using BattleShip.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip.ViewModels
+{
+    internal class GameViewModel : BaseViewModel
+    {
+    }
+}
---
+++ BattleShip/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using BattleShip.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip.ViewModels
+{
+    internal class MainViewModel : BaseViewModel
+    {
+        public BaseViewModel CurrentViewModel { get; set; } = new GameViewModel(); 
+    }
+}
